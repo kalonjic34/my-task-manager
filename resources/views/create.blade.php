8dd5bd00bfe8e7446480c7157671372a608567c8
Christian <kalonjic34@gmail.com>
2026-08-24 14:24:16 +0200

    Refactor task creation form labels and placeholders for clarity

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -15,30 +15,30 @@
     <form action="{{ route('tasks.store') }}" method="POST">
         @csrf
         <div>
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title">
+            <label for="title">Task name</label>
+            <input type="text" name="title" id="title" value="{{ old('title') }}">
             @error('title')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
 
         <div>
-            <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5"></textarea>
+            <label for="description">Brief description</label>
+            <textarea name="description" id="description" rows="5"{{ old('description') }}></textarea>
             @error('description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
         <div>
-            <label for="long_description">Long Description</label>
-            <textarea name="long_description" id="long_description" rows="10"></textarea>
+            <label for="long_description">Detailed description</label>
+            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
             @error('long_description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
 
         <div>
-            <button type="submit">Add Task</button>
+            <button type="submit">Create task</button>
         </div>
     </form>
     
