c2e0717dcca34bc88323cc069075afb372553347
Christian <kalonjic34@gmail.com>
2026-08-24 15:01:40 +0200

    Update TaskRequest authorization and clean up validation rules

--- app/Http/Requests/TaskRequest.php
+++ app/Http/Requests/TaskRequest.php
@@ -7,23 +7,20 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class TaskRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            
+        'title'=> 'required|max:255',
+        'description'=> 'required',
+        'long_description'=> 'required',
+    
         ];
     }
 }
