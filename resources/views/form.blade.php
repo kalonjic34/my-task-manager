5266bb9226daed69ce98639e037495b9b0334e10
Christian <kalonjic34@gmail.com>
2026-08-24 15:31:43 +0200

    Add form view for task creation and editing with validation messages

---
+++ resources/views/form.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('title', isset($task) ? 'Edit Task':'Add Task')
+
+@section('styles')
+<style >
+    .error-message{
+        color:red;
+        font-size: 0,10rem;
+    }
+</style>
+@endsection
+
+@section('content')
+    <form action="{{ isset($task) ? route('tasks.update',['task'=> $task->id]):route('tasks.store') }}" method="POST">
+        @csrf
+        @isset($task)
+        @method('PUT')
+            
+        @endisset
+        <div>
+            <label for="title">Task name</label>
+            <input type="text" name="title" id="title" value="{{ $task->title ??  old('title') }}">
+            @error('title')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div>
+            <label for="description">Brief description</label>
+            <textarea name="description" id="description" rows="5"{{ $task->description ?? old('description') }}></textarea>
+            @error('description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <label for="long_description">Detailed description</label>
+            <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description ?? old('long_description')  }}</textarea>
+            @error('long_description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div>
+            <button type="submit">@isset($task)
+                Update Task
+                @else
+                Create Task
+            @endisset</button>
+        </div>
+    </form>
+    
+@endsection
