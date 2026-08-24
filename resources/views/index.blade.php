bc553a09f76fa5c166b7f5fdd0bf4a8e96916311
Christian <kalonjic34@gmail.com>
2026-08-24 16:11:46 +0200

    Enhance task index view with navigation and task creation link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'Your Personal Task Overview')
 
 @section('content')
+
+<div>
+    <a href="{{ route('tasks.create')}}">Add Task!</a>
+</div>
     
     @forelse ($tasks as $task)
         <div>
@@ -11,5 +15,11 @@
     @empty
         <div>You have no tasks yet.<div>
     @endforelse 
+
+    @if ($task ->count())
+        <nav>
+                {{ $tasks->links() }} 
+        </nav>
+    @endif
 
 @endsection
