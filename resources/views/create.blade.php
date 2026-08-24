47c9909beaceb9c19884b4dbdc8c3f51698ed5a9
Christian <kalonjic34@gmail.com>
2026-08-24 15:31:29 +0200

    Refactor create view to include form component

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,45 +1,7 @@
 @extends('layouts.app')
 
-@section('title','Add Task')
-
-@section('styles')
-<style>
-    .error-message{
-        color:red;
-        font-size: 0,10rem;
-    }
-</style>
-@endsection
 
 @section('content')
-    <form action="{{ route('tasks.store') }}" method="POST">
-        @csrf
-        <div>
-            <label for="title">Task name</label>
-            <input type="text" name="title" id="title" value="{{ old('title') }}">
-            @error('title')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div>
-            <label for="description">Brief description</label>
-            <textarea name="description" id="description" rows="5"{{ old('description') }}></textarea>
-            @error('description')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            <label for="long_description">Detailed description</label>
-            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
-            @error('long_description')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div>
-            <button type="submit">Create task</button>
-        </div>
-    </form>
+@include('form')
     
 @endsection
