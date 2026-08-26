61e97d121c2ef8850cb5e5d7510dd4114806a19f
Christian <kalonjic34@gmail.com>
2026-08-26 14:13:18 +0200

    Refactor task index view layout for improved navigation and styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,20 +4,23 @@
 
 @section('content')
 
-<div>
-    <a href="{{ route('tasks.create')}}">Add Task!</a>
-</div>
+<nav class="mb-4">
+    <a href="{{ route('tasks.create')}}" class="font-medium text-grey-700 underline decoration-pink-500">Add Task!</a>
+</nav>
     
     @forelse ($tasks as $task)
         <div>
-            <a href="{{ route('tasks.show',['task'=>$task-> id]) }}">{{ $task->title }}</a>
+            <a href="{{ route('tasks.show',['task'=>$task-> id]) }}"
+                
+                @class(['line-through'=> $task->completed])>{{ $task->title }}</a>
+        clas
         </div>  
     @empty
         <div>You have no tasks yet.<div>
     @endforelse 
 
     @if ($task ->count())
-        <nav>
+        <nav class="mt-4">
                 {{ $tasks->links() }} 
         </nav>
     @endif
